46c96485968ba49cfacda3db68718f588688fc98
Keita-Yamada310 <japaussie78@yahoo.co.jp>
2026-07-18 16:03:50 +0900

    Set DATAPIPE_EXPERIMENT_ID to experiment ID

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // DataPipe experiment ID="B24M5IgU2YMf"
 // 例: const DATAPIPE_EXPERIMENT_ID = "abc123";
 // 空欄のままなら、終了時にCSVを参加者のPCへダウンロードします。
-const DATAPIPE_EXPERIMENT_ID = "";
+const DATAPIPE_EXPERIMENT_ID = "B24M5IgU2YMf";
 
 // 1項目の制限時間（ミリ秒）
 const TRIAL_DURATION_MS = 10000;
