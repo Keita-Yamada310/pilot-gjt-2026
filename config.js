7f53ff4614d80a04375cca06e72920db4568954f
Keita-Yamada310 <japaussie78@yahoo.co.jp>
2026-07-19 16:34:45 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// DataPipe experiment ID
+const DATAPIPE_EXPERIMENT_ID = "B24M5IgU2YMf";
+
+// 1項目の制限時間（ミリ秒）
+const TRIAL_DURATION_MS = 10000;
+
+// 30項目の提示順を参加者ごとにランダム化
+const RANDOMIZE_ITEMS = true;
+
+// パイロット課題の項目数
+const TOTAL_GJT_ITEMS = 30;
